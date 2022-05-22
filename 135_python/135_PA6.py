e09b5fbbea642bbd8b080aa3db4b377e67bf8b2f
Bill <btong@nevada.unr.edu>
2022-05-21 23:49:55 -0700

    PA6 finished 5/21

--- 135_python/135_PA6.py
+++ 135_python/135_PA6.py
@@ -9,21 +9,46 @@ def getWords():
     count = 0
     for x in words_array:
         count += 1
-
+    return words_array, count
 
 def countLetters():
-    pass
+    lcount = 0
+    for x in words:
+        for i in x:
+            if i.isalpha():
+                lcount += 1
+    return lcount
 
 def countLowerCase():
-    pass
+    lowCount = 0
+    for x in words:
+        for i in x:
+            if i.islower():
+                lowCount += 1
+    return lowCount
 
 def countVowels():
-    pass
+    vcount = 0
+    for x in words:
+        for i in x:
+            if i.lower() in ("a", "e", "i", "o", "u"):
+                vcount += 1
+            elif i.upper() in ("A", "E", "I", "O", "U"):
+                vcount += 1
+    return vcount
 
 name = "poem.txt"
 if os.path.exists(name):
     f = open(name, "r")
-    getWords()
+    results = getWords()
+    words = results[0]
+    num_words = results[1]
     f.close()
+    num_letters = countLetters()
+    print("Number of letters: " + str(num_letters))
+    num_lowercase = countLowerCase()
+    print("Number of lowercase: " + str(num_lowercase))
+    num_vowels = countVowels()
+    print("Number of vowels: " + str(num_vowels))
 else:
     print(f"The file {name} does not exist. ")
