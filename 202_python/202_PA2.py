7eee125981aa4e4f2f9f0ca1d31f25a48ef11dda
Bill <btong@nevada.unr.edu>
2022-06-06 00:46:14 -0700

    PA2 in progress 6/6

--- 202_python/202_PA2.py
+++ 202_python/202_PA2.py
@@ -1,20 +1,37 @@
 import os.path
+global number, lname, fname, gpa
+# Classes are not helpful for this particular assignment in python
 
-class students():
-    def __init__(self, fname, lname, number, gpa ):
-        self.fname = fname
-        self. lname = lname
-        self.number = number
-        self.gpa = gpa
+def getStudents():
+    with open(name, "r") as f:
+        lines = f.readlines()
+        count = 0
+        for x in lines:
+            number.append(x.split(" ")[0])
+            lname.append(x.split(" ")[1])
+            fname.append(x.split(" ")[2])
+            gpa.append(x.split()[3])
+            count += 1
+        return count
 
-    def getStudents(self):
-        pass
-    def findMaxStudent(self):
-        pass
+def findMaxStudent():
+    highest_gpa = 0
+    for x in gpa:
+        if x > x-1:
+            highest_gpa = x
+    print(highest_gpa)
+
+def printStudentTable():
+    pass
 
 name = "students.txt"
 if os.path.exists(name):
-    f = open(name, "r")
-    f.close()
+    number = []
+    lname = []
+    fname = []
+    gpa = []
+    student_count = getStudents()
+    findMaxStudent()
+
 else:
     print(f"The file {name} does not exist.")
