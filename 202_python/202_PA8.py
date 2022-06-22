56ec816aed76a1bd4097659d6377133436f4ea9e
Bill <btong@nevada.unr.edu>
2022-06-21 21:04:31 -0700

    PA8 finished

--- 202_python/202_PA8.py
+++ 202_python/202_PA8.py
@@ -9,20 +9,43 @@ class Calculator:
         self.operand2 = input("Second operand: ")
 
     def addition(self):
-        return self.operand1 + self.operand2
+        return int(self.operand1) + int(self.operand2)
 
     def subtraction(self):
-        return self.operand1 - self.operand2
+        return int(self.operand1) - int(self.operand2)
 
     def multiplication(self):
-        return self.operand1 * self.operand2
+        return int(self.operand1) * int(self.operand2)
 
     def division(self):
-        return self.operand1 / self.operand2
+        return int(self.operand1) / int(self.operand2)
 
 class Complex:
-    pass
+    def __init__(self, operand1, operand2):
+        self.operand1 = operand1
+        self.operand2 = operand2
 
+    def getOperands(self):
+        self.operand1 = input("First operand: ")
+        self.operand2 = input("Second operand: ")
+
+    def addition(self):
+        try:
+            return int(self.operand1) + int(self.operand2)
+        except:
+            return self.operand1 + "+" + self.operand2
+
+    def subtraction(self):
+        try:
+            return int(self.operand1) - int(self.operand2)
+        except:
+            return self.operand1 + "-" + self.operand2
+
+    def multiplication(self):
+        try:
+            return int(self.operand1) * int(self.operand2)
+        except:
+            return self.operand1 + "*" + self.operand2
 
 def getCalcType():
     print("CALCULATOR MODES")
@@ -34,21 +57,55 @@ def getCalcType():
     return input()
 
 def getOperation():
-    pass
+    print("1.   Addition")
+    print("2.   Subtraction")
+    print("3.   Multiplication")
+    print("4.   Division")
+    return int(input())
 
 def integerCalculator():
-    pass
+    opChoice = getOperation()
+    set_operands.getOperands()
+    if opChoice == 1:
+        print(set_operands.addition())
+    elif opChoice == 2:
+        print(set_operands.subtraction())
+    elif opChoice == 3:
+        print(set_operands.multiplication())
+    elif opChoice == 4:
+        print(set_operands.division())
+    else:
+        print("Invalid operation.")
 
 def complexCalculator():
-    pass
+    opChoice = getOperation()
+    set_complex.getOperands()
+    if opChoice == 1:
+        print(set_complex.addition())
+    elif opChoice == 2:
+        print(set_complex.subtraction())
+    elif opChoice == 3:
+        print(set_complex.multiplication())
+    elif opChoice == 4:
+        print("This complex Calculator does not support division.")
+    else:
+        print("Invalid operation.")
+
+def floatingCalculator():
+    print("In python 3, integers are converted to floats as needed.")
+    print("As a result, the standard integer calculator acts as a floating calculator when needed.")
+    print("Redirecting to integer Calculator...")
 
 userChoice = getCalcType()
+set_operands = Calculator(None, None)
+set_complex = Complex(None, None)
 if int(userChoice) == 1:
-    pass
+    integerCalculator()
 elif int(userChoice) == 2:
-    pass
+    floatingCalculator()
+    integerCalculator()
 elif int(userChoice) == 3:
-    pass
+    complexCalculator()
 elif int(userChoice) == 0:
     raise SystemExit
 else:
